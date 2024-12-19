b973b66b7c10a03b7e789bcc9d7c86d0a4527136
xq <546247083@qq.com>
2024-12-19 09:28:54 +0800

    组件视图

---
+++ AspNetCore/MVCDemo/MVCDemo/ViewComponents/MovieList.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVCDemo.Data.DataContext;
+using MVCDemo.Data.Model;
+
+namespace MVCDemo.ViewComponents
+{
+    public class MovieList : ViewComponent
+    {
+        private readonly MVCDemoContext _context;
+
+        public MovieList(MVCDemoContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string componentName, int maxCount)
+        {
+            var items = await GetItemsAsync(maxCount);
+            return View(componentName, items);
+        }
+
+        private Task<List<Movie>> GetItemsAsync(int maxCount)
+        {
+            return _context!.Movie!.Take(maxCount).ToListAsync();
+        }
+    }
+}
